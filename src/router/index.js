70c3f5284c50b7ca3c142381b838ead87fabf53f
v_wkvwang <v_wkvwang@tencent.com>
2021-06-02 21:25:04 +0800

    完成底部tabbar

--- src/router/index.js
+++ src/router/index.js
@@ -2,18 +2,51 @@ import vue from 'vue'
 import VueRouter from 'vue-router'
 
 // 对相应的文件进行懒加载
-const Home = () => import()
+const Home = () => import('../views/home/Home')
+const Category = () => import('../views/category/Category')
+const Cart = () => import('../views/cart/Cart')
+const Profile = () => import('../views/profile/Profile')
+const Detail = () => import('../views/detail/Detail')
 
 // 安装插件
 vue.use(VueRouter)
 
 // 创建路由 配置路由的映射关系
 const routes = [
-
+  {
+    path: '',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/category',
+    name: 'category',
+    component: Category
+  },
+  {
+    path: '/cart',
+    name: 'cart',
+    component: Cart
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: Profile
+  },
+  {
+    path: '/detail/:iid',
+    component: Detail
+  }
 ]
 
 const router = new VueRouter({
   routes,
   mode: 'history'
 })
+
+export default router
 
---
+++ src/views/profile/Profile.vue
@@ -0,0 +1,9 @@
+<template>
+  <h2></h2>
+</template>
+
+<script>
+export default {
+  name: "Profile"
+}
+</script>
