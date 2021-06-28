5c247ceb14606950c07a32f75fa1c1930c42c039
v_wkvwang <v_wkvwang@tencent.com>
2021-06-28 20:22:18 +0800

    px2vw-css 单位转换插件

--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ Vue.config.productionTip = false
 //事件总线（非父子组件之间传递东西）
 Vue.prototype.$bus=new Vue()
 
+// 解决移动端点击延迟300毫秒的解决办法
+import FastClick from 'fastclick'
+FastClick.attach(document.body)
+
 //vue图片懒加载
 import VueLazyLoad from 'vue-lazyload'
 // 设置懒加载占位符
